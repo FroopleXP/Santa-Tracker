8baa6af45fb7eed70725f5624cef47dfbe0e7ca4
Connor D. Edwards <connor@frooplexp.com>
2020-11-14 00:56:19 +0000

    Updated README

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
 import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="santa-tracker-frooplexp",
+    version="1.0.0",
+    author="Connor D. Edwards",
+    author_email="connor@frooplexp.com",
+    description="Basic Raspberry PI based Santa Tracker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FroopleXP/Santa-Tracker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Santa Tracker :: Google"
+    ],
+    python_requires='>=3.6',
+)
